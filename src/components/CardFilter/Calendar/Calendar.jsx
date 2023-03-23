ae098f65a051f5e67302d1371d34d49000a790a9
ihsanbrahomov98 <ihsanbrahomov@abv.bg>
2023-03-23 17:57:18 +0200

    refactoring / MainPage Calendar updated

--- src/components/CardFilter/Calendar/Calendar.jsx
+++ src/components/CardFilter/Calendar/Calendar.jsx
@@ -1,38 +1,40 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import { addDays } from "date-fns";
-import "react-datepicker/dist/react-datepicker.css";
-import "./calendar.css";
-// CSS Modules, react-datepicker-cssmodules.css
-// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+// import React, { useState } from "react";
+// import DatePicker from "react-datepicker";
+// import { addDays } from "date-fns";
+// import "react-datepicker/dist/react-datepicker.css";
+// import "./calendar.css";
+// // CSS Modules, react-datepicker-cssmodules.css
+// // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-const Calendar = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(null);
-  const onChange = (dates) => {
-    const [start, end] = dates;
-    setStartDate(start);
-    setEndDate(end);
-  };
-  return (
-    <div className="d-flex justify-content-center">
-      <DatePicker
-        selected={startDate}
-        onChange={onChange}
-        startDate={startDate}
-        endDate={endDate}
-        excludeDates={[
-          addDays(new Date(), 1),
-          addDays(new Date(), 5),
-          addDays(new Date(), 2),
-          addDays(new Date(), 4),
-        ]}
-        selectsRange
-        selectsDisabledDaysInRange
-        inline
-      />
-    </div>
-  );
-};
+// const Calendar = () => {
+//   const [startDate, setStartDate] = useState(new Date());
+//   const [endDate, setEndDate] = useState(null);
+//   const onChange = (dates) => {
+//     const [start, end] = dates;
+//     setStartDate(start);
+//     setEndDate(end);
+//   };
+//   return (
+//     <div className="d-flex justify-content-center mt-4">
+//       <div className="mt-2">
+//         <DatePicker
+//           selected={startDate}
+//           onChange={onChange}
+//           startDate={startDate}
+//           endDate={endDate}
+//           excludeDates={[
+//             addDays(new Date(), 1),
+//             addDays(new Date(), 5),
+//             addDays(new Date(), 2),
+//             addDays(new Date(), 4),
+//           ]}
+//           selectsRange
+//           selectsDisabledDaysInRange
+//           inline
+//         />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Calendar;
+// export default Calendar;
