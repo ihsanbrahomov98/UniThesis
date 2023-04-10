ddc09bdb43dafa03474196080f071fea59301d87
ihsanbrahomov98 <ihsanbrahomov@abv.bg>
2023-04-10 12:01:22 +0300

    adding more fields

--- src/components/RegisterSitter/Body/Body.jsx
+++ src/components/RegisterSitter/Body/Body.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./body.css";
 import { Link } from "react-router-dom";
+import { ChevronDown } from "react-bootstrap-icons";
+import { CityArray } from "../../../Enums/CityEnum/CityEnum";
 
 const Body = () => {
   const [formState, setFormState] = useState({
@@ -34,9 +36,18 @@ const Body = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
     setFormState((prevState) => ({ ...prevState, [name]: value }));
   };
 
+  const handleClick = (dataInfo, dataType) => {
+    console.log(formState);
+    setFormState((prevState) => ({
+      ...prevState,
+      [dataType]: dataInfo,
+    }));
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -97,32 +108,61 @@ const Body = () => {
             <div className="col-6">
               {" "}
               <div className=" d-flex justify-content-start align-items-center flex-column p-4">
-                <div className="mb-1">
+                <div className="mb-1 w-100">
                   <input
                     type="text"
                     className={`form-control ${
                       validationState.name ? "is-invalid" : ""
                     }`}
+                    placeholder="Име"
                     id="name"
                     name="name"
                     value={formState.name}
                     onChange={handleChange}
                   />
                 </div>
-                <div className="mb-1">
+                <div className="mb-1 w-100">
+                  <input
+                    placeholder="Фамилия"
+                    type="text"
+                    className={`form-control ${
+                      validationState.surName ? "is-invalid" : ""
+                    }`}
+                    id="surName"
+                    name="surName"
+                    value={formState.surName}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-1 w-100">
+                  <input
+                    placeholder="Телефонен номер"
+                    type="text"
+                    className={`form-control ${
+                      validationState.telephone ? "is-invalid" : ""
+                    }`}
+                    id="telephone"
+                    name="telephone"
+                    value={formState.telephone}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-1 w-100">
                   <input
                     type="email"
                     className={`form-control ${
                       validationState.email ? "is-invalid" : ""
                     }`}
+                    placeholder="Емайл"
                     id="email"
                     name="email"
                     value={formState.email}
                     onChange={handleChange}
                   />
                 </div>
-                <div className="mb-1">
+                <div className="mb-1 w-100">
                   <input
+                    placeholder="Парола"
                     type="password"
                     className={`form-control ${
                       validationState.password ? "is-invalid" : ""
@@ -133,50 +173,103 @@ const Body = () => {
                     onChange={handleChange}
                   />
                 </div>
+                <div className="mb-1 w-100">
+                  <input
+                    placeholder="Потвърди паролата"
+                    type="password"
+                    className={`form-control ${
+                      validationState.confirmPassword ? "is-invalid" : ""
+                    }`}
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    value={formState.confirmPassword}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
             </div>
             <div className="col-6">
               {" "}
               <div className="d-flex justify-content-start align-items-center flex-column p-4">
-                <input
-                  type="text"
-                  className="form-control mb-1"
-                  placeholder="Потребителско име"
-                  aria-label="Потребителс"
-                />
-                <input
-                  type="password"
-                  className="form-control mb-1"
-                  placeholder="Парола"
-                  aria-label="Парола"
-                />
-                <input
-                  type="password"
-                  className="form-control mb-1"
-                  placeholder="Потвърди парола"
-                  aria-label="Потвърди парола"
-                />{" "}
-                <input
-                  type="email"
-                  className="form-control mb-1"
-                  placeholder="Емайл"
-                  aria-label="Емайл"
-                />{" "}
-                <input
-                  type="text"
-                  className="form-control mb-1"
-                  placeholder="Телефон"
-                  aria-label="Телефон"
-                />
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Телефон"
-                  aria-label="Телефон"
-                />
+                <div className="mb-1 w-100">
+                  <input
+                    type="text"
+                    className={`form-control ${
+                      validationState.name ? "is-invalid" : ""
+                    }`}
+                    placeholder="Име"
+                    id="name"
+                    name="name"
+                    value={formState.name}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-1 w-100">
+                  <input
+                    type="text"
+                    className={`form-control ${
+                      validationState.name ? "is-invalid" : ""
+                    }`}
+                    placeholder="Име"
+                    id="name"
+                    name="name"
+                    value={formState.name}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="d-flex flex-row dropdown mt-1 w-100 ">
+                  <button
+                    type="button"
+                    className="d-flex justify-content-between btn border w-100 "
+                    data-bs-toggle="dropdown"
+                    value={formState.city}
+                  >
+                    {formState.city}
+                    <div className="d-flex flew-row"></div>
+                    <div className="">
+                      <ChevronDown />
+                    </div>
+                  </button>
+
+                  <ul className="dropdown-menu CardFilterItemWidth">
+                    <li
+                      className="dropdown-item w-100 d-flex CardFilterItemCursor"
+                      onClick={() => handleClick(CityArray.SOFIA, "city")}
+                    >
+                      {CityArray.SOFIA}
+                    </li>
+                    <li
+                      className="dropdown-item w-100 d-flex CardFilterItemCursor"
+                      onClick={() => handleClick(CityArray.PLOVDIV, "city")}
+                    >
+                      {CityArray.PLOVDIV}
+                    </li>
+                    <li
+                      className="dropdown-item w-100 d-flex CardFilterItemCursor"
+                      onClick={() => handleClick(CityArray.BURGAS, "city")}
+                    >
+                      {CityArray.BURGAS}
+                    </li>
+
+                    <li
+                      className="dropdown-item w-100 d-flex CardFilterItemCursor"
+                      onClick={() => handleClick(CityArray.VARNA, "city")}
+                    >
+                      {CityArray.VARNA}
+                    </li>
+                    <li
+                      className="dropdown-item w-100 d-flex CardFilterItemCursor"
+                      onClick={() => handleClick(CityArray.SMOLYAN, "city")}
+                    >
+                      {CityArray.SMOLYAN}
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
+
           <div className="w-100 d-flex justify-content-center">
             <button
               type="submit"
